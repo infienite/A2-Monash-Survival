0c80829534566e1ecc6a4b6bc1377ac3e81dbae4
infienite <khilfikhairulamin@gmail.com>
2026-05-06 20:11:26 +1000

    I implemented it wrongly using ArraySortedList :_\

--- location_manager.py
+++ location_manager.py
@@ -1,4 +1,5 @@
-from campus import Campus
+from campus import Campus, Location, Connection
+from data_structures import ArraySortedList, LinkedList
 
 class LocationManager:
     def __init__(self, campus_name):
@@ -6,24 +7,54 @@ class LocationManager:
         Analyse your time complexity of this method.
         """
         self.campus: Campus = Campus(campus_name)
+        
+        all_location = self.campus.get_all_locations()
+
+        self.location_desireability = ArraySortedList(len(all_location))
+
+        for location in all_location:
+            location: Location = location
+            total_diff = 0
+            conns = location.get_connections()
+            for conn in conns:
+                conn: Connection = conn
+                total_diff += conn.get_difficulty()
+            avg_diff = 0 if len(conns) == 0 else total_diff / len(conns)
+            reward = location.get_reward()
+            desirability = round(reward / (1 + avg_diff), 2)
+            self.location_desireability.add((desirability, location.get_name()))
 
     def get_locations_in_range(self, min_score, max_score):
         """
         Analyse your time complexity of this method.
         """
-        pass
+        ranged_location_desireability = LinkedList()
+        n = len(self.location_desireability)
+        i = 0
+        while i < n and self.location_desireability[i][0] < min_score:
+            i += 1
+        
+        while i < n and self.location_desireability[i][0] <= max_score:
+            ranged_location_desireability.append(self.location_desireability[i])
+            i += 1
+
+        return ranged_location_desireability
 
     def get_top_k_locations(self, k):
         """
         Analyse your time complexity of this method.
         """
-        pass
+        top_location_desireability = LinkedList()
+        n = len(self.location_desireability)
+        for j in range(k):
+            top_location_desireability.append(self.location_desireability[n-1-j])
+        return top_location_desireability
 
     def update_location(self, name, new_reward):
         """
         Analyse your time complexity of this method.
         """
-        pass
+        
 
     def __str__(self):
         """
@@ -33,5 +64,8 @@ class LocationManager:
 
 if __name__ == '__main__':
     location_manager = LocationManager('clayton')
-
+    print(location_manager.location_desireability)
+    print()
+    print(location_manager.get_locations_in_range(.86, 2.4))
+    print(location_manager.get_top_k_locations(3))
     # Add test code here
