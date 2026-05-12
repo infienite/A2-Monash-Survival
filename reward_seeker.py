7314b391b0e3dcea2f51429e51624121460c5396
infienite <khilfikhairulamin@gmail.com>
2026-05-13 00:02:40 +1000

    Complete Task 4 presub

--- reward_seeker.py
+++ reward_seeker.py
@@ -1,57 +1,118 @@
 from campus import Campus, Location
-from data_structures import HashTableSeparateChaining, ArrayList, ArrayR, LinkedList
-from reward_structure import ArrayMaxHeap
+from data_structures import ArrayR, LinkedList
+from reward_structure import UniqueArrayMaxHeap
+
+
+def get_heap_key(location: Location):
+    """ Take the location `reward` and `name` as heap key.  """
+    return (location.get_reward(), location.get_name())
+
+
+def validate_k_argument(k: int, ):
+    """ Validate `k` argument. `k` must be a positive integer. """
+    if type(k) != int and k < 0:
+        raise ValueError('k argument must be a positive integer')
+
 
 class RewardSeeker:
     def __init__(self, campus_name):
         """
-        Analyse your time complexity of this method.
+        Best and worst case time complexity is O(N) where N is the number of locations.
+        Best and worst case happens when N locations are added into the heap. When the heap
+        is initialized, the heap is built using bottom-up construction. The copy operation
+        takes O(N) time to copy the array elements into a heap array. The sink operation
+        on the other hand places takes O(log N) because it traverse through each level of the
+        tree to place sink element to the correct position. This operation only occurs to
+        log N nodes, and the complexity for sink operation on log N nodes is ignored.
         """
         self.campus: Campus = Campus(campus_name)
-        locs: LinkedList[Location] = self.campus.get_all_locations()
-        arr: ArrayR = ArrayR(len(locs))
-        self.lookup = HashTableSeparateChaining(len(locs))
-        
+
+        # Get all locations
+        all_locations: LinkedList[Location] = self.campus.get_all_locations()
+
+        # Convert locations list from LinkedList to ArrayR
+        all_locations_arr: ArrayR = ArrayR(len(all_locations))
         i = 0
-        for loc in locs:
-            arr[i] = (loc.get_reward(), loc.get_name())
-            self.lookup[loc.get_name()] = loc.get_reward()
+        for location in all_locations:
+            all_locations_arr[i] = get_heap_key(location)
             i += 1
 
-        # print(arr)
-        self.heap = ArrayMaxHeap(len(locs), arr)
+        # Initialize max heap
+        self.reward_max_heap = UniqueArrayMaxHeap(len(all_locations), all_locations_arr)
 
     def get_next_location(self):
         """
-        Analyse your time complexity of this method.
+        Best and worst case time complexity is O(log N) where N is the number of elements
+        inside the heap.
+        Best and worst case happens when there are N elements inside the heap. The maximum
+        element is always at the root of the heap and takes O(1). The removal of the element
+        causes sink operation to be performed in order to maintain heap invariant of the
+        maximum element being at the top of the max heap. Sink operation takes O(log N)
+        as the the tree is traversed at log N level.  
         """
-        return self.heap.get_max()
+        # Get max element inside the heap
+        return self.reward_max_heap.get_max()
 
     def get_top_k_locations(self, k):
         """
-        Analyse your time complexity of this method.
+        Best and worst case time complexity is O(k log N) where k is the argument provided and
+        N is the number of elements inside the heap.
+        Best and worst case happens when there are N elements inside the heap and k <= N. The
+        max element is removed from the heap taking log N time for k times in total. When
+        k > N, the function will halts after N time and takes O(k log N) where k = N.
         """
-        o = LinkedList()
+        # Validate argument
+        validate_k_argument(k)
+
+        # Output list
+        top_k_locations = LinkedList()
+        
+        # Call get max for k time if k < N. Else call for N time.
         for _ in range(k):
-            o.append(self.heap.get_max())
-        return o
+            # Add location if the heap is not empty
+            try:
+                top_k_locations.append(self.reward_max_heap.get_max())
+            except IndexError:
+                break
+
+        # Return output list
+        return top_k_locations
 
     def update_location_reward(self, location_name, new_reward):
         """
-        Analyse your time complexity of this method.
+        Best case time complexity is O(1).
+        Best case happens when the new location reward is equal to the current location reward.
+        The update function of the heap halts early because no changes occurred to the node and
+        thus no update need to be done to the structure of the heap.
+
+        Worst case time complexity is O(log N) where N is the number of elements inside the heap.
+        Worst case happens when there are N elements inside the heap and the new reward is more
+        than or less than the current reward. When the new reward is larger, the rise operation
+        swaps the heap element with its parent for up to log N times until it reaches the correct
+        position. When the new reward is smaller, the sink operation swaps the heap element with
+        its child up to log N times or the depth of the tree until reaches the correct position.
         """
-        r = self.lookup[location_name]
-        self.heap.update((r, location_name), (new_reward, location_name))
-        loc: Location = self.campus.get_location_by_name(location_name)
-        loc.set_reward(new_reward)
+        # Get location by name
+        location = self.campus.get_location_by_name(location_name)        
+        current_key = get_heap_key(location)
+
+        # Update location reward
+        location.set_reward(new_reward)
+
+        # Get new key of
+        new_key = get_heap_key(location)
+        
+        # Update the new reward inside the max heap
+        self.reward_max_heap.update(current_key, new_key)
+
 
 if __name__ == '__main__':
     reward_seeker = RewardSeeker('clayton')
 
-    # print(reward_seeker.get_top_k_locations(3))
-    reward_seeker.update_location_reward('Monash Club', 1000)
-    print(reward_seeker.get_top_k_locations(3))
-    reward_seeker.update_location_reward('Campus Centre', 67)
     print(reward_seeker.get_next_location())
+    # reward_seeker.update_location_reward('Monash Club', 1000)
+    print(reward_seeker.get_top_k_locations(3))
+    reward_seeker.update_location_reward('Campus Centre', 100)
+    print(reward_seeker.get_top_k_locations(2))
 
     # Add test code here
--- reward_structure.py
+++ reward_structure.py
@@ -1,160 +1,190 @@
 from data_structures import ArrayR, HashTableSeparateChaining
 
-class ArrayMaxHeap(ArrayR):
-    MIN_CAPACITY = 1
 
-    def __init__(self, max_size: int, an_array: ArrayR = None, verbose=False) -> None:
-        """
-        If an_array is specified then the elements of the future 
-        heap are known in advance.
-        Assume that max_size=len(an_array) if given.
-        """
+class UniqueArrayMaxHeap:
+    _MIN_CAPACITY = 1
 
-        # playing with the arguments to set up the length
-        if an_array is None:
-            self._length = 0 # missing on page 126 of 20-Heaps2.pdf
-        else:
-            self._length = max_size = len(an_array) 
+    def __init__(self, max_size: int, initial_heap_elements: ArrayR = None) -> None:
+        """ Initialize a unique max heap using array. Applies bottom-up construction to fill with element inside array or start with empty heap if no array provided. """
+        # Initialize the empty array heap
+        self._heap_array = ArrayR(max(self._MIN_CAPACITY, max_size) + 1)
         
-        # allocate the array
-        self._the_array = ArrayR(max(self.MIN_CAPACITY, max_size) + 1)
+        # Hash table to track element index inside the array
+        self._index_lookup = HashTableSeparateChaining(max(self._MIN_CAPACITY, max_size))
         
-        self.lookup = HashTableSeparateChaining(self._length)
+        # When no array provided, keep empty heap
+        if initial_heap_elements is None:
+            self._length = 0
+            return
         
-        # if an_array is given then apply bottom-up heap construction
-        if an_array:
-            self.heapify(an_array, verbose)
+        # Heapify the array using bottom-up construction
+        self._length = len(initial_heap_elements)
+        self._heapify(initial_heap_elements)
 
-
+    def _is_unique(self, elem) -> bool:
+        """ Return true if element is not inside the heap. """
+        # Index is not unique if it already exist inside the hash table
+        try:
+            _ = self._index_lookup[str(elem)]
+            return False
+        
+        # Index is unique if it's not inside hash table
+        except KeyError:
+            return True
+        
+    def _validate_element(self, elem):
+        """ Raise error if element already exists. """
+        if not self._is_unique(elem):
+            raise ValueError(f'The element {str(elem)} already exists inside the heap.')
+    
     def __len__(self) -> int:
+        """ Return number of element inside heap array. """
         return self._length
 
     def is_full(self) -> bool:
-        return self._length + 1 == len(self._the_array)
+        """ Checks if the number of element has reached maximum capacity of the heap. """
+        return self._length + 1 == len(self._heap_array)
 
-    def heapify(self, an_array: ArrayR, verbose=False) -> None:
-        """
-        Apply bottom-up heap construction in O(n) time.
-        """
-
-        # copy an_array to self._the_array (shift by 1)
+    def _heapify(self, from_array: ArrayR) -> None:
+        """ Use bottom-up heap construction to build the heap. """
+        # Copy the array into heap array but leave index 0 empty
         for i in range(self._length):
-            self._the_array[i + 1] = an_array[i]
-            self.lookup[str(an_array[i])] = i + 1
+            self._validate_element(from_array[i])
+            self._heap_array[i + 1] = from_array[i]
+            self._index_lookup[str(from_array[i])] = i + 1
 
-        if verbose:
-            print('the_array before bottom-up heap construction')
-            for i in self._the_array:
-                print(i)
-
-        # heapify every parent
+        # Heapify parents of the tree
         for i in range(self._length // 2, 0, -1):
-            if verbose:
-                print('sinking the parent', i)
+            self._sink(i)
 
-            k = self.sink(i)
+    def _rise(self, k: int) -> None:
+        """ Rise element at index k to its correct position. """
+        # Element heap array
+        elem = self._heap_array[k]
 
-            if verbose:
-                for i in self._the_array:
-                    print(i)
-
-        if verbose:
-            print('the_array after bottom-up heap construction')
-            for i in self._the_array:
-                print(i)
-
-    def rise(self, k: int) -> None:
-        """
-        Rise element at index k to its correct position
-        :pre: 1 <= k <= self._length
-        """
-        item = self._the_array[k]
-
-        while k > 1 and item > self._the_array[k // 2]:
-            self._the_array[k] = self._the_array[k // 2]
-            self.lookup[str(self._the_array[k // 2])] = k
+        # Rise element if element is larger than its parent until reaches correct position
+        while k > 1 and elem > self._heap_array[k // 2]:
+            self._heap_array[k] = self._heap_array[k // 2]
+            self._index_lookup[str(self._heap_array[k // 2])] = k
             k = k // 2
-        self._the_array[k] = item
-        self.lookup[str(item)] = k
+        
+        # Update element and index lookup
+        self._heap_array[k] = elem
+        self._index_lookup[str(elem)] = k
 
     def add(self, element) -> bool:
-        """
-        Swaps elements while rising
-        """
+        """ Swaps elements while rising. """
+        # Cannot add more element when tree is full
         if self.is_full():
             raise IndexError
-
-        self._length += 1
-        self._the_array[self._length] = element
-        self.rise(self._length)
-
-    def largest_child(self, k: int) -> int:
-        """
-        Returns the index of k's child with greatest value.
-        :pre: 1 <= k <= self._length // 2
-        """
         
+        # Validate element
+        self._validate_element(element)
+
+        # Update length and insert new element at end of array
+        self._length += 1
+        self._heap_array[self._length] = element
+        
+        # Rise the new element to the correct position
+        self._rise(self._length)
+
+    def _largest_child(self, k: int) -> int:
+        """ Returns the index of k's child with greatest value. """
         if 2 * k == self._length or \
-                self._the_array[2 * k] > self._the_array[2 * k + 1]:
+                self._heap_array[2 * k] > self._heap_array[2 * k + 1]:
             return 2 * k
         else:
             return 2 * k + 1
 
-    def sink(self, k: int) -> None:
-        """ Make the element at index k sink to the correct position.
-            :pre: 1 <= k <= self._length
-            :complexity: ???
-        """
-        item = self._the_array[k]
+    def _sink(self, k: int) -> None:
+        """ Make the element at index k sink to the correct position. """
+        item = self._heap_array[k]
 
+        # Sink element if element is smaller than its child until reaches correct position
         while 2 * k <= self._length:
-            max_child = self.largest_child(k)
-            if self._the_array[max_child] <= item:
+            max_child = self._largest_child(k)
+            if self._heap_array[max_child] <= item:
                 break
-            self._the_array[k] = self._the_array[max_child]
-            self.lookup[str(self._the_array[max_child])] = k
+            self._heap_array[k] = self._heap_array[max_child]
+            self._index_lookup[str(self._heap_array[max_child])] = k
             k = max_child
 
-        self._the_array[k] = item
-        self.lookup[str(item)] = k
-        return k
+        # Update element and index lookup
+        self._heap_array[k] = item
+        self._index_lookup[str(item)] = k
         
     def get_max(self):
         """ Remove (and return) the maximum element from the heap. """
+        # Cannot get max element when there is no element
         if self._length == 0:
             raise IndexError
 
-        max_elt = self._the_array[1]
-        del self.lookup[str(max_elt)]
+        # Get maximum element of heap
+        max_elem = self._heap_array[1]
+
+        # Delete the index lookup of element
+        del self._index_lookup[str(max_elem)]
+
+        # Update length
         self._length -= 1
+
+        # Ensure max element at the top by sinking the element if it's lower than its child
         if self._length > 0:
-            self._the_array[1] = self._the_array[self._length+1]
-            self.sink(1)
-        return max_elt
+            self._heap_array[1] = self._heap_array[self._length+1]
+            self._sink(1)
+        
+        # Return max element
+        return max_elem
     
     def update(self, elem, new_elem):
-        # print(self.lookup.keys())
-        k = self.lookup[str(elem)]
-        self._the_array[k] = new_elem
+        """ Update the element to a new value. """
 
+        # Halts early if the current element is the same as the new element
         if elem == new_elem:
             return
+
+        # Validate argument
+        self._validate_element(new_elem)
+
+        # Update the element at that index
+        k = self._index_lookup[str(elem)]
+        self._heap_array[k] = new_elem
+        
+        # Rise when new element is larger than the previous value
         if new_elem > elem:
-            self.rise(k)
+            self._rise(k)
+        
+        # Sink when new element is smaller than the previous value
         elif new_elem < elem:
-            self.sink(k)
-        del self.lookup[str(elem)]
+            self._sink(k)
+        
+        # Delete the old index lookup to the element with the previous value
+        del self._index_lookup[str(elem)]
+
 
 if __name__ == '__main__':
     #items = [ int(x) for x in input('Enter a list of numbers: ').strip().split() ]
     items = [ 1, 9, 12, 5, 17, 15, 2]
-    heap = ArrayMaxHeap(len(items), items)
+    max_heap = UniqueArrayMaxHeap(len(items), items)
+
+    print(max_heap.get_max())
+
+    max_heap.update(5, 19)
+
+    print(max_heap.get_max())
+
+    max_heap.update(12, 20)
+    max_heap.update(9, 21)
+    max_heap.update(1, 22)
+    max_heap.update(2, 24)
+
+    print(max_heap.get_max())
+    print(max_heap.get_max())
+    print(max_heap.get_max())
+    print(max_heap.get_max())
     
-    for i in heap._the_array:
-        print("Item", i)
+    max_heap.add(4)
 
-    print()
-
-    # output all elements of the heap in descending order
-    while(len(heap) > 0):
-        print(heap.get_max())
+    max_heap.update(4, 200)
+    print(max_heap.get_max())
+    print(max_heap.get_max())
