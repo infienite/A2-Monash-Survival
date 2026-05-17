fdb32b35559b652fd4d7cdd5fca175d3966c7499
infienite <khilfikhairulamin@gmail.com>
2026-05-17 14:08:23 +1000

    Complete Task 3 correctly

--- location_manager.py
+++ location_manager.py
@@ -1,11 +1,16 @@
 from campus import Campus, Location
-from data_structures import LinkedList, BinarySearchTree, ArrayList, HashTableSeparateChaining
+from data_structures import (
+    LinkedList,
+    BinarySearchTree,
+    ArrayList,
+    HashTableSeparateChaining,
+)
 from data_structures.node_binary import BinaryNode
 from algorithms import merge_sort
 
 
 def calculate_desirability(location: Location) -> float:
-    """ Calculate desirability of a location. """
+    """Calculate desirability of a location."""
     # Get all connections of the location
     conns = location.get_connections()
 
@@ -13,25 +18,27 @@ def calculate_desirability(location: Location) -> float:
     total_diff = 0
     for conn in conns:
         total_diff += conn.get_difficulty()
-    
+
     # Calculate average difficulty. If no connection, average difficulty set to 0.
     avg_diff = 0 if len(conns) == 0 else total_diff / len(conns)
-    
+
     # Calculate desirability = reward / (1 + average difficulty)
     reward = location.get_reward()
     return round(reward / (1 + avg_diff), 2)
 
 
 def validate_score_argument(score: int | float):
-    """ Raise value error if score is invalid type. """
+    """Raise value error if score is invalid type."""
     if type(score) != int and type(score) != float:
-        raise ValueError('score argument must be a number')
-    
+        raise ValueError("score argument must be a number")
+
 
 def validate_k_argument(k: int, num_of_locations: int):
-    """ Raise value error if k is invalid type . """
-    if type(k) != int or not(k > 0 and k <= num_of_locations):
-        raise ValueError('k argument must be a positive integer whose value is between 1 and number of locations (inclusive).')
+    """Raise value error if k is invalid type ."""
+    if type(k) != int or not (k > 0 and k <= num_of_locations):
+        raise ValueError(
+            f"k argument must be a positive integer whose value is between 1 and {num_of_locations}."
+        )
 
 
 class LocationManager:
@@ -39,7 +46,7 @@ class LocationManager:
         """
         Best and worst case time complexity is O(C + N log N) where N is the number of locations while C is the number of
         connections of the campus.
-        Best and worst case happens when there are N interconnected locations with C connections. The function will
+        Best and worst case happens when there are N interconnected locations and C connections. The function will
         calculate the desirability of each location which requires iterating through all N locations and in each
         of the iteration, the function will loop through all connection's difficulty level Ci times in total where
         i represent node i and Ci represent the number of connection of node i. After that, the function will run
@@ -49,14 +56,14 @@ class LocationManager:
         """
         # Get campus data
         self.campus: Campus = Campus(campus_name)
-        
+
         # Get locations list
         all_location = self.campus.get_all_locations()
 
         # Hash table for looking up desirability value based on location name
         self.desirability_lookup = HashTableSeparateChaining(len(all_location))
 
-        # Calculate desirability of each location. Store in lookup
+        # Calculate desirability of each location. Store in lookup (Note: complexity of this for loop is O(N+C) as it visits all N locations and C connections once)
         for location in all_location:
 
             # Calculate desirability of the location
@@ -65,25 +72,29 @@ class LocationManager:
             # Add into lookup and sorted list
             self.desirability_lookup.insert(location.get_name(), desirability)
 
-        # Initialize search tree that searches location based on desirability 
+        # Initialize search tree that searches location based on desirability
         self.location_search_tree = BinarySearchTree()
 
-        def create_binary_tree_aux(sorted_location: ArrayList[Location], i: int, j: int) -> BinaryNode:
-            """ Builds binary tree by taking middle element as the root, then divides the list into left and right partitions as left and right child respectively. """
+        def create_binary_tree_aux(
+            sorted_location: ArrayList[Location], i: int, j: int
+        ) -> BinaryNode:
+            """Builds binary tree by taking middle element as the root, then divides the list into left and right partitions as left and right child respectively."""
             # Base case. The left index is more than right index.
             if i > j:
                 return None
-            
+
             # Find middle index
             mid = (i + j) // 2
 
             # Add desirability and location into the tree
             location = sorted_location[mid]
-            self.location_search_tree[self.desirability_lookup[location.get_name()]] = location
+            self.location_search_tree[self.desirability_lookup[location.get_name()]] = (
+                location
+            )
 
             # Builds left and right child
-            create_binary_tree_aux(sorted_location, i, mid-1)
-            create_binary_tree_aux(sorted_location, mid+1, j)
+            create_binary_tree_aux(sorted_location, i, mid - 1)
+            create_binary_tree_aux(sorted_location, mid + 1, j)
 
         # Convert from LinkedList to ArrayList for sorting
         all_location_arrlist = ArrayList(len(all_location))
@@ -91,10 +102,12 @@ class LocationManager:
             all_location_arrlist.append(location)
 
         # Sort locations before building binary search tree
-        sorted_locations = merge_sort.merge_sort(all_location_arrlist, key=lambda x: self.desirability_lookup[x.get_name()])
+        sorted_locations = merge_sort.merge_sort(
+            all_location_arrlist, key=lambda x: self.desirability_lookup[x.get_name()]
+        )
 
         # Build binary search tree based on desirability as the key
-        create_binary_tree_aux(sorted_locations, 0, len(sorted_locations)-1)
+        create_binary_tree_aux(sorted_locations, 0, len(sorted_locations) - 1)
 
     def get_locations_in_range(self, min_score, max_score):
         """
@@ -120,24 +133,25 @@ class LocationManager:
             # Base case. Key is larger than the max score
             if location[0] > max_score:
                 break
-            
+
             # When key is in range, append to output list
             if location[0] >= min_score:
                 locations_in_range.append((location[0], location[1].get_name()))
-        
+
         return locations_in_range
 
     def get_top_k_locations(self, k):
         """
         Best and worst case time complexity is O(K + log N) where K is the integer representing
         the number of locations to return from the function and N is the number of locations inside
-        the tree. 
-        Best and worst case happens when the search happens from the root of the tree to the rightmost
-        leaf from the root which takes O(log N) time. Since the rightmost leaf contains the largest
+        the tree.
+        Best and worst case happens when the search happens when the root of the tree is traversed to the
+        rightmost leaf from the root which takes O(log N) time. Since the rightmost leaf contains the largest
         desirability value, the function adds the leaf node to the output list. Then, the function
-        returns the counter for the previous function call to signal that k-1 location need to be added
-        to the output list. This process happens for K time in total. After the final top location
-        has been added, the function will stop adding location to the output list.
+        returns counter-1 to the previous function call to signal that counter-1 location left needed to
+        be added to the output list. After that, the left node is visited if coutner-1 > 0. This process
+        happens for K time in total. After the final top location has been added, the function will stop adding 
+        location to the output list and stop visiting left node.
         """
         # Validate argument
         validate_k_argument(k, len(self.location_search_tree))
@@ -145,25 +159,26 @@ class LocationManager:
         # Output list
         top_k_locations = LinkedList()
 
-        def get_top_k_locations_aux(node: BinaryNode) -> int:
-            """ Fills the output list with top K locations from the tree. """
+        def get_top_k_locations_aux(node: BinaryNode, counter: int) -> int:
+            """Fills the output list with top K locations from the tree."""
             # Go to next rightmost child when the node is not empty
             if node:
                 # Go to next location and wait for counter value return
-                counter = get_top_k_locations_aux(node.right)
+                updated_counter = get_top_k_locations_aux(node.right, counter)
 
                 # Add the location into the output list if the counter is still more than 0
-                if counter > 0:
+                if updated_counter > 0:
                     top_k_locations.append((node.key, node.item.get_name()))
+                    updated_counter = get_top_k_locations_aux(node.left, updated_counter-1)
 
-                # Return counter - 1 to signal previous function to either continue or stop adding element into the output list. 
-                return counter-1
+                # Return counter - 1 to signal previous function to either continue or stop adding element into the output list.
+                return updated_counter
             # Base case. Return k to signal previous function to start adding element into output list.
             else:
-                return k
-            
+                return counter
+
         # Build the output list
-        get_top_k_locations_aux(self.location_search_tree._root)
+        get_top_k_locations_aux(self.location_search_tree._root, k)
 
         # Return the top K locations
         return top_k_locations
@@ -186,16 +201,16 @@ class LocationManager:
         try:
             desirability = self.desirability_lookup[name]
         except KeyError:
-            raise ValueError(f'name of location does not exist')
-        
+            raise ValueError(f"name of location does not exist")
+
         # Halts early if new reward is equal to previous reward
         reward = self.campus.get_location_by_name(name).get_reward()
         if new_reward == reward:
             return
-        
+
         # Get the location by name
         location: Location = self.location_search_tree[desirability]
-        
+
         # Update location reward
         location.set_reward(new_reward)
 
@@ -210,20 +225,21 @@ class LocationManager:
 
         # Update lookup table with new desirability
         self.desirability_lookup[name] = new_desirability
-        
+
     def __str__(self):
         """
         Optional: For debugging purposes only
         """
         pass
 
-if __name__ == '__main__':
-    location_manager = LocationManager('clayton')
-    print()
-    print(location_manager.get_locations_in_range(0, 0.86))
-    print()
-    print(location_manager.get_top_k_locations(4))
-    location_manager.update_location('Law Building and Library', 14)
-    print(location_manager.get_locations_in_range(1.5, 6))
+
+if __name__ == "__main__":
+    location_manager = LocationManager("clayton")
+    print(location_manager.get_locations_in_range(0.87, 1000))
+    print(location_manager.get_top_k_locations(5))
+    location_manager.update_location("Law Building and Library", 14)
+    print(location_manager.get_locations_in_range(0.86, 6))
+    print(location_manager.get_top_k_locations(10))
+
 
     # Add test code here
