63bef7d592074572adc5541bdb20d9f4a81a1210
infienite <khilfikhairulamin@gmail.com>
2026-05-17 12:53:37 +1000

    Done Task 2

--- leaderboard.py
+++ leaderboard.py
@@ -4,21 +4,21 @@ from data_structures import ArrayList
 
 
 def is_leaderboard_sorted(leaderboard: ArrayList[Player]) -> bool:
-    """ Checks whether the leaderboard is already sorted. Returns `True` if the leaderboard is sorted. Else, return `False`. """
-    
+    """Checks whether the leaderboard is already sorted. Returns `True` if the leaderboard is sorted. Else, return `False`."""
+
     # Set the initial score and stamina as a very big number
-    prev_score, prev_stamina = float('inf'), float('inf')
+    prev_score, prev_stamina = float("inf"), float("inf")
 
     # Checks loop invariant holds or not at each iteration
     for i in range(len(leaderboard)):
-        
+
         # Get player score and stamina
         player_i = leaderboard[i]
         cur_score, cur_stamina = player_i.score, player_i.stamina
 
         # Loop invariant. The current score and stamina must be strictly less than or equal to the previous score and stamina.
         if cur_score <= prev_score and cur_stamina <= prev_stamina:
-            
+
             # Keeps updating the value to compare next element with the current element
             prev_score = cur_score
             prev_stamina = cur_stamina
@@ -26,7 +26,7 @@ def is_leaderboard_sorted(leaderboard: ArrayList[Player]) -> bool:
         else:
             # Loop invariant breached
             return False
-    
+
     # All loop invariant fulfilled
     return True
 
@@ -43,14 +43,13 @@ class Leaderboard:
         Worst case time complexity is O(N log N) where N is the length of the list.
         Worst case happens when the leaderboard undergoes mergesort operation. The list will
         be divided by half until each partition becomes pair of 2 elements. This takes about
-        O(N log N) as the length of the list is divided by 2 each iteration and in each 
+        O(N log N) as the length of the list is divided by 2 each iteration and in each
         iteration, all N elements are visited to be copied into a new, smaller array. The merge
         operation takes O(N) time as in each iteration, the each element of the array
         will be merged into a bigger array. As the array length starts expanding by a factor of
         2 as more elements merge, the final array can be achieved in O(log N) steps of
         iteration. So, it takes O(N log N) as well.
         """
-
         # Get leaderboard data
         self.players: ArrayList[Player] = LeaderboardReader.read(campus_name)
 
@@ -58,8 +57,10 @@ class Leaderboard:
         if is_leaderboard_sorted(self.players):
             return
 
-        # Sort in descending order based on score and stamina 
-        self.players = merge_sort.merge_sort(self.players, lambda p: (-p.score, -p.stamina))
+        # Sort in descending order based on score and stamina in descending order
+        self.players = merge_sort.merge_sort(
+            self.players, lambda p: (-p.score, -p.stamina)
+        )
 
     def combine(self, other_leaderboard: Leaderboard):
         """
@@ -68,10 +69,12 @@ class Leaderboard:
         Best and worst case happens when both leaderboard lists are sorted descendingly from the
         player with the highest score and stamina to the lowest. Each element is only visited
         once and added to the final list which contains the combined leaderboard in descending
-        order. 
+        order.
         """
-        # Merge two leaderboards
-        self.players = merge_sort.merge(self.players, other_leaderboard.players, lambda p: (-p.score, -p.stamina))
+        # Merge two leaderboards in descending order prioritizing the first list items for equal keys of the first and second list
+        self.players = merge_sort.merge(
+            self.players, other_leaderboard.players, lambda p: (-p.score, -p.stamina)
+        )
 
     def __str__(self):
         """
@@ -97,12 +100,23 @@ if __name__ == "__main__":
     # exit(0)
 
     leaderboard = Leaderboard("clayton")
-    for player in leaderboard.players:
-        print(player)
-    other_leaderboard = Leaderboard("malaysia")
-    # print(other_leaderboard.players)
+    # for player in leaderboard.players:
+    #     print(player)
 
-    leaderboard.combine(other_leaderboard)
-    for player in leaderboard.players:
-        print(player)
-    # Add test code here
+    # ids = [4,7,2,10,8,3,1,6,9,5]
+    # for i in range(len(leaderboard.players)):
+    #     assert ids[i] == leaderboard.players[i].id
+
+    # other_leaderboard = Leaderboard("malaysia")
+    # # print(other_leaderboard.players)
+    # ids = [20,17,12,16,14,11,18,13,19,15]
+    # for i in range(len(other_leaderboard.players)):
+    #     assert ids[i] == other_leaderboard.players[i].id
+
+    # leaderboard.combine(other_leaderboard)
+    # for player in leaderboard.players:
+    #     print(player)
+
+    # ids = [4, 7, 2, 20, 17, 12,10,16,8,14,11,3,18,1,6,13,9,19,15,5]
+    # for i in range(len(leaderboard.players)):
+    #     assert ids[i] == leaderboard.players[i].id
